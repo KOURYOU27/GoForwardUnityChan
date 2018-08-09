8118aadacb180e56e80530374d6a08376155923a
RyokoAraki <a-ry.27@poem.ocn.ne.jp>
2018-08-09 09:16:26 +0900

    GoForwardUnityChan　課題提出
    ブロック同士と、ブロックと地面が衝突した時に音がでるように出来ました。

---
+++ Assets/CubeSound.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeSound : MonoBehaviour {
+
+    AudioSource audiosource;
+    public AudioClip audioClip;
+
+    // Use this for initialization
+    void Start () {
+
+        audiosource = gameObject.GetComponent<AudioSource>();
+        audiosource.clip = audioClip;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    //衝突時に呼ばれる関数
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "CubeTag" || other.gameObject.tag == "GroundTag")
+        {
+            audiosource.Play();
+        }
+
+    }
+
+
+
+}
